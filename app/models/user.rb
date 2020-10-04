db63f5e40c0223d67589c3a5ff7aa064c7099dc3
zahidalik <zahidkhan545@icloud.com>
2020-10-04 16:02:42 +0300

    Add rolify gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,32 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :trackable,
          :confirmable
 
+  rolify
+
   def to_s
     email
   end
 
   def username
     self.email.split(/@/).first
+  end
+
+  # after_create :assign_default_role
+  # def assign_default_role
+  #   self.add_role(:student) if self.roles.blank?
+  # end
+
+  # ADVANCED FORM OF THE ABOVE CODE
+  after_create :assign_default_role
+  def assign_default_role
+    if User.count == 1
+      self.add_role(:admin) if self.roles.blank?
+      self.add_role(:teacher)
+      self.add_role(:student)
+    else
+      self.add_role(:student) if self.roles.blank?
+      self.add_role(:teacher) # if you want any user to be able to create own courses 
+    end
   end
 
   has_many :courses
