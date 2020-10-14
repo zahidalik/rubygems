69bfdb910960dbd8227edf887ceb8e39f4bdc39c
zahidalik <zahidkhan545@icloud.com>
2020-10-14 20:50:15 +0300

    Change new and create actions in enrollments

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,6 +1,6 @@
 class EnrollmentsController < ApplicationController
   before_action :set_enrollment, only: [:show, :edit, :update, :destroy]
-
+  before_action :set_course, only: [:new, :create]
   # GET /enrollments
   # GET /enrollments.json
   def index
@@ -24,17 +24,12 @@ class EnrollmentsController < ApplicationController
   # POST /enrollments
   # POST /enrollments.json
   def create
-    @enrollment = Enrollment.new(enrollment_params)
-    @enrollment.price = @enrollment.course.price
-    
-    respond_to do |format|
-      if @enrollment.save
-        format.html { redirect_to @enrollment, notice: 'Enrollment was successfully created.' }
-        format.json { render :show, status: :created, location: @enrollment }
-      else
-        format.html { render :new }
-        format.json { render json: @enrollment.errors, status: :unprocessable_entity }
-      end
+    if @course.price > 0
+      flash[:alert] = "You can not access paid courses yet."
+      redirect_to new_course_enrollment_path(@course)
+    else
+      @enrollment = current_user.buy_course(@course)
+      redirect_to course_path(@course), notice: "You are enrolled!"
     end
   end
 
@@ -64,6 +59,10 @@ class EnrollmentsController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
+    def set_course
+      @course = Course.friendly.find(params[:course_id])
+    end
+    
     def set_enrollment
       @enrollment = Enrollment.find(params[:id])
     end
