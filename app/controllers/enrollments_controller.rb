59f48d89cd72aabd5d2802707b721f3786a6626b
zahidalik <zahidkhan545@icloud.com>
2020-10-17 18:30:27 +0300

    Add friendly_id to enrollments

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -69,7 +69,7 @@ class EnrollmentsController < ApplicationController
     end
 
     def set_enrollment
-      @enrollment = Enrollment.find(params[:id])
+      @enrollment = Enrollment.friendly.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
