6d4739af6c0c26d9daa6cd24e44d8399581df19d
zahidalik <zahidkhan545@icloud.com>
2021-04-25 16:50:07 +0300

    Add a counter cache to users and lessons for the comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,6 +12,18 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @course = Course.friendly.find(params[:course_id])
+    @lesson = Lesson.friendly.find(params[:lesson_id])
+    @comment = Comment.find(params[:id])
+    if @comment.destroy
+      respond_to do |format|
+        format.html { redirect_to course_lesson_path(@course, @lesson), notice: 'Comment was successfully destroyed.' }
+        format.json { head :no_content }
+      end
+    end
+  end
+
   private
 
   def comment_params
